2d5d65d24155a2c12574cca59295e42e8fa310be
avikghatak <ag.19u10018@btech.nitdgp.ac.in>
2021-06-27 03:56:55 +0530

    ft: home and compose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Home } from './components/Home/Home Jsx/Home';
+import {Compose} from './components/Compose/Compose Jsx/Compose'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      Hello
+      <BrowserRouter>
+      <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/compose' component={Compose} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
