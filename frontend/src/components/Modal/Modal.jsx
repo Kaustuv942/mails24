36368b2001b484818432a23c8e0fc8a0a9dabdcc
avikghatak <ag.19u10018@btech.nitdgp.ac.in>
2021-06-27 17:21:36 +0530

    add Modal files

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,18 +1,33 @@
 import "./Modal.css";
-const Modal = ({ recipient, cc, subject, body }) => {
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
+const Modal = ({ recipient, cc, subject, body, showModal, setShowModal }) => {
+  //   console.log(showModal);
+  let dummyShow = showModal;
   return (
-    <div className="ModalContainer container">
+    <div
+      className="ModalContainer container"
+      style={{ display: showModal ? "block" : "none" }}
+    >
+      <FaTimes
+        className="cut"
+        onClick={() => {
+          setShowModal(!dummyShow);
+        }}
+      />
       <div className="ModalHead">
-        <h1>{recipient}</h1>
+        <p>To : {recipient}</p>
       </div>
       <div className="Modalcc">
-        <h2>{cc}</h2>
+        <p>cc : {cc}</p>
       </div>
-      <div className="ModalSubject">
-        <h2>{subject}</h2>
-      </div>
-      <div className="ModalBody">
-        <h3>{body}</h3>
+      <div className="ModalMain">
+        <div className="ModalSubject">
+          <h4>Subject : {subject}</h4>
+        </div>
+        <div className="ModalBody">
+          <h5>{body}</h5>
+        </div>
       </div>
     </div>
   );
