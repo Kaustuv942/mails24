b1870b5bf62e23b9b3d01801960e0781f6593314
Kaustuv <kaustuv942@gmail.com>
2021-06-28 01:49:40 +0530

    fix deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => res.send("Welcome to Express 1.0"));
 
