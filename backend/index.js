814b897aa24c6eafc4f61e79e599eca3c4a58be7
Kaustuv <kaustuv942@gmail.com>
2021-06-27 12:27:10 +0530

    feat: compose api

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const schedule = require('node-schedule');
 
 const User = require('./models/User');
+const Mail = require('./models/Mail');
 
 let app = express();
 app.use(cors());
@@ -31,15 +33,6 @@ client.connect(err => {
 });
 
 const nodemailer = require('nodemailer');
-
-
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'personal.kkc942@gmail.com',
-//     pass: 'joker@123' // naturally, replace both with your real credentials or an application-specific password
-//   }
-// });
 let transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
@@ -50,31 +43,6 @@ let transporter = nodemailer.createTransport({
       clientSecret: 'V2WxstHlYNrnASVYR5Efv7g_'
   }
 });
-// const mailOptions = {
-//   from: 'personal.kkc942@gmail.com',
-//   to: 'kaustuv942@gmail.com',
-//   // cc:'kkc.19u10500@btech.nitdgp.ac.in',
-//   // bcc:'kaustuv942@gmail.com',
-//   subject: 'Flipr Mailer Works',
-//   text: 'Let us get this up and running'
-// };
-
-/// THIS WORKS::
-// transporter.sendMail({
-//   from: 'apsingh1843@gmail.com',
-//   to: 'kaustuv942@gmail.com',
-//   // cc:'kkc.19u10500@btech.nitdgp.ac.in',
-//   // bcc:'kaustuv942@gmail.com',
-//   subject: 'Flipr Mailer Works: Revision 2',
-//   text: 'Send Mail Api Works',
-//   auth: {
-//       user: 'apsingh1843@gmail.com',
-//     //   refreshToken: '1//04PeaRRxQepXfCgYIARAAGAQSNwF-L9IroDPofc1F4M8a0E-scC550DnVy5h_hOAs9WowULZYaUW42owSILtat4EYUK0YS-_5NKQ',
-//       accessToken: 'ya29.a0AfH6SMBgm-vsyqZ8zw9HHs4wX2V4zm3csaRmKTLWyDRDIhmf0AUlK02v3bbetDCC_tHcUhsxg1B-Sra913vwlXIWCCJ1N0XBqgJyo4PucRmyDKsOLGepkhs1STiprvOxcBJUPt5c_ZPXJsrgSHVNLPD850bL',
-      
-//       // expires: 1484314697598
-//   }
-// });
 
 
 
@@ -86,54 +54,6 @@ let transporter = nodemailer.createTransport({
 //   }
 // });
 
-console.log('Scheduling')
-// Scheduling :
-
-var cron = require('node-cron');
-var x= 1;
-var y = 1;
-const url_taskMap = {};
-const str = '* * * * * *'
-// const task = cron.schedule(str,()=>{
-//     console.log('sendMail'+x)
-//     x+=1
-// });
-// url_taskMap['xyz'] = task;
-// const task3 = cron.schedule('*/3 * * * * *',()=>{
-//     console.log('3Seconds test'+x)
-//     x+=1
-// });
-// url_taskMap['xyz'] = task;
-// for some condition in some code
-// const stopper = cron.schedule(' */5 * * * * *',()=>{
-//     let my_job = url_taskMap['xyz'];
-//     console.log(my_job)
-//     console.log(url_taskMap)
-//     my_job.stop();
-//     console.log('Stopped the current task')
-// });
-// url_taskMap['3seconds'] = task3;
-
-// const stopper3 = cron.schedule(' */20 * * * * *',()=>{
-//     let my_job = url_taskMap['3seconds'];
-//     my_job.stop();
-//     console.log('Stopped the current task3')
-// });
-
-/* 
-POST REQ:
-/sendMail
-
----> mail info
----> mail scheduling(3 opts)
----> In each of the options:
-1. initialize the job 
-2. store it in the db as follows:
-a. url_taskmap --> 
-3. take the task and store it under the user's history of schedules:
-
-
-*/
 mongoose.connect(uri,{
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -219,3 +139,248 @@ app.post('/api/login', (req, res) => {
         })
    })
 });
+
+//google-login ~ gmail sync:
+/* 
+
+access-code:
+email:
+
+
+*/ 
+
+app.post('/api/gmailsync', (req, res) => {
+    //res.send('login');
+    const gId = req.body.profileObj.googleId
+    const email = req.body.profileObj.email
+    const name = req.body.profileObj.name
+    const imageUrl = req.body.profileObj.imageUrl
+    const accessToken = req.body.accessToken
+  
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          // New user creation, no email field
+          const newUser = new User({
+            gId,
+            name,
+            imageUrl,
+            email,
+            accessToken,
+          });
+          newUser.gId = gId;
+          newUser.email = email
+          newUser.name = name
+          newUser.imageUrl = imageUrl
+          newUser.accessToken = accessToken
+          newUser.save()
+          console.log('Created New User')
+        }
+        else{
+          // use the same doc
+          user.gId = gId;
+          user.email = email
+          user.name = name
+          user.imageUrl = imageUrl
+          user.accessToken = accessToken
+          user.save()
+          console.log('updated existing User')
+        } 
+        res.status(200).json({
+          gId: gId
+        })
+     });
+  });
+
+// NOTE: Access token Expiry error needs to be resolved. 
+
+//compose message && scheduling
+
+app.post('/api/compose', (req, res) => {
+  /* Eg json
+  {
+    mail:{
+      to: ""
+      from: ""
+      cc...
+    }
+    schedule:{
+      type: "recurring/weekly/monthly/yearly"
+      start: "upto frontend peeps"
+      max: "no of max mails, Hard-Limit of 100"
+    }
+  }
+  */
+  const {to, from, cc, bcc, subject, text} = req.body.mail
+  const {type, start, max} = req.body.schedule
+  const newMail = new Mail({
+    from,
+    to,
+    cc,
+    bcc,
+    subject,
+    text
+  });
+  newMail.from = from
+  newMail.to = to
+  // newMail.cc = cc
+  // newMail.bcc = bcc
+  newMail.subject = subject
+  newMail.text = text
+  const email = from
+  User.findOne({email})
+    .then(user => {
+      console.log(from)
+      console.log(user)
+      // if(!user){
+
+      // }
+      if(!user.gId || user.gId == null){
+        res.json({
+          msg: "Gmail not synced!"
+        })
+      }
+      else{ 
+        newMail.save()
+        if(type === null){ // Instant Send
+          // send mail 1 time
+          transporter.sendMail({
+            from: newMail.from,
+            to: newMail.to,
+            cc: newMail.cc,
+            bcc: newMail.bcc,
+            subject: newMail.subject,
+            text: newMail.text,
+            auth: {
+                user: user.email,
+                accessToken: user.accessToken,
+            }
+          });
+          
+          res.json({
+            msg: "The Mail is sent successfully"
+          })
+        }
+        else if(type === 'Recurring'){      // Starting Now
+          // 
+          const taskId = "MY_CUSTOM_ID"
+          console.log('With Start-time')
+          const job = schedule.scheduleJob(taskId, '*/30 * * * * *', function () {
+            console.log('running campaign: ' + taskId)
+            transporter.sendMail({
+              from: newMail.from,
+              to: newMail.to,
+              cc: newMail.cc,
+              bcc: newMail.bcc,
+              subject: newMail.subject,
+              text: newMail.text,
+              auth: {
+                  user: user.email,
+                  accessToken: user.accessToken,
+              }
+            });
+          })
+          setTimeout(function(){ 
+            const campaignId = "MY_CUSTOM_ID"
+            let current_job = schedule.scheduledJobs[campaignId]
+            current_job.cancel()
+          }, 450000);
+          console.log(job)
+          res.json({
+            code: "OK"
+          })
+          
+        }
+        else{
+          res.json('Work In Progress')
+          console.log('With Start-time')
+        }
+      }
+    });
+  
+  
+  //-> save to db(1), start cron job -> store cron-job,[, ]
+  // next time of sending the mail db(1),
+  // Store in history whenever a cron job is being done ~>
+  // [History]db(2) (last time of sending, number of times sent till now, type of mail)
+  // Schedule(future): when the job is introduced for the first time
+  // It is inserted into the schedule db with (next time of sending, type of sending, number of mails remaining)
+})
+
+var x= 1;
+var y = 1;
+const url_taskMap = {};
+const str = '* * * * * *'
+// const task = cron.schedule(str,()=>{
+//     console.log('sendMail'+x)
+//     x+=1
+// });
+// url_taskMap['xyz'] = task;
+// const task3 = cron.schedule('*/3 * * * * *',()=>{
+//     console.log('3Seconds test'+x)
+//     x+=1
+// });
+// url_taskMap['xyz'] = task;
+// for some condition in some code
+// const stopper = cron.schedule(' */5 * * * * *',()=>{
+//     let my_job = url_taskMap['xyz'];
+//     console.log(my_job)
+//     console.log(url_taskMap)
+//     my_job.stop();
+//     console.log('Stopped the current task')
+// });
+// url_taskMap['3seconds'] = task3;
+
+// const stopper3 = cron.schedule(' */20 * * * * *',()=>{
+//     let my_job = url_taskMap['3seconds'];
+//     my_job.stop();
+//     console.log('Stopped the current task3')
+// });
+
+/* 
+POST REQ:
+/sendMail
+
+---> mail info
+---> mail scheduling(3 opts)
+---> In each of the options:
+1. initialize the job 
+2. store it in the db as follows:
+a. url_taskmap --> 
+3. take the task and store it under the user's history of schedules:
+
+
+*/
+// const mailOptions = {
+//   from: 'personal.kkc942@gmail.com',
+//   to: 'kaustuv942@gmail.com',
+//   // cc:'kkc.19u10500@btech.nitdgp.ac.in',
+//   // bcc:'kaustuv942@gmail.com',
+//   subject: 'Flipr Mailer Works',
+//   text: 'Let us get this up and running'
+// };
+
+/// THIS WORKS::
+// transporter.sendMail({
+//   from: 'apsingh1843@gmail.com',
+//   to: 'kaustuv942@gmail.com',
+//   // cc:'kkc.19u10500@btech.nitdgp.ac.in',
+//   // bcc:'kaustuv942@gmail.com',
+//   subject: 'Flipr Mailer Works: Revision 2',
+//   text: 'Send Mail Api Works',
+//   auth: {
+//       user: 'apsingh1843@gmail.com',
+//     //   refreshToken: '1//04PeaRRxQepXfCgYIARAAGAQSNwF-L9IroDPofc1F4M8a0E-scC550DnVy5h_hOAs9WowULZYaUW42owSILtat4EYUK0YS-_5NKQ',
+//       accessToken: 'ya29.a0AfH6SMBgm-vsyqZ8zw9HHs4wX2V4zm3csaRmKTLWyDRDIhmf0AUlK02v3bbetDCC_tHcUhsxg1B-Sra913vwlXIWCCJ1N0XBqgJyo4PucRmyDKsOLGepkhs1STiprvOxcBJUPt5c_ZPXJsrgSHVNLPD850bL',
+      
+//       // expires: 1484314697598
+//   }
+// });
+
+
+
+//get history
+
+//get schedules
+
+//user-data
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,18 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  }
+    gId:{
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: false,
+    },
+    imageUrl:{
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    accessToken: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: false,
+    }
 });
 module.exports = User = mongoose.model('user', UserSchema);
